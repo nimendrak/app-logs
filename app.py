85cb2c927d54e637a6484af0f3b40a1560808fd4
nimendrak <nimendra.2019264@iit.ac.lk>
2021-12-14 14:57:45 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import logging
+import logging.config
+from pythonjsonlogger import jsonlogger
+from datetime import datetime;
+
+class ElkJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(ElkJsonFormatter, self).add_fields(log_record, record, message_dict)
+        log_record['@timestamp'] = datetime.now().isoformat()
+        log_record['level'] = record.levelname
+        log_record['logger'] = record.name
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger("MainLogger")
+
+logging.info('Application running!')
